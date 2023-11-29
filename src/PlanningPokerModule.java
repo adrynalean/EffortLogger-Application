84e264b8028fc24a58057a9e5e3e64e153a43f46
adrynalean <87601800+adrynalean@users.noreply.github.com>
2023-11-29 10:24:07 -0700

    commit requirememnt
    Hey This is the commit for the planningpoker module

--- src/PlanningPokerModule.java
+++ src/PlanningPokerModule.java
@@ -21,6 +21,7 @@ public class PlanningPokerModule extends VBox {
     private ObservableList<String> cardOptions = FXCollections.observableArrayList( "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
 
     public PlanningPokerModule() {
+        //initializing omponents
         initializeComponents();
     }
 
